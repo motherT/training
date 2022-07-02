76f1d29f2fedeae8915c18af7020fec0720adfa4
ravi991 <raviranjancs4@gmail.com>
2022-07-02 06:28:59 -0400

    Add files via upload

---
+++ testngPackage/OpenWebsites.java
@@ -0,0 +1,44 @@
+package testngPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenWebsites {
+WebDriver driver;
+	@Test
+    public void openingAmazonSite() throws InterruptedException {
+        // for running in Chrome, uncomment 2 following lines
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+        driver = new ChromeDriver();
+//        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Administrator\\Documents\\drivers\\geckodriver.exe");
+//        driver = new FirefoxDriver();
+        System.out.println("Open /close  amazon.com in chrome browser");
+        driver.get("http://www.amazon.com/");
+        Thread.sleep(2000);
+        driver.close();
+    }
+    @Test
+    public void openingCNNSite() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+        driver = new ChromeDriver();
+//        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Administrator\\Documents\\drivers\\geckodriver.exe");
+//        driver = new FirefoxDriver();
+        System.out.println("Open /close  cnn.com in chrome browser");
+        driver.get("http://www.cnn.com/");
+        Thread.sleep(2000);
+        driver.close();
+    }
+    @Test
+    public void openingYahooSite() throws InterruptedException {
+        
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+        driver = new ChromeDriver();
+//        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Administrator\\Documents\\drivers\\geckodriver.exe");
+//        driver = new FirefoxDriver();
+        System.out.println("Open /close  yahoo.com in chrome browser");
+        driver.get("http://www.yahoo.com/");
+        Thread.sleep(2000);
+        driver.close();
+    }
+}
