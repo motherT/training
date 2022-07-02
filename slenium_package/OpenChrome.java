5940e02cd8d25ef93b178f0b9becb5f5931c6618
ravi991 <raviranjancs4@gmail.com>
2022-07-02 06:06:26 -0400

    Add files via upload

---
+++ slenium_package/OpenChrome.java
@@ -0,0 +1,14 @@
+package seleniumPackage;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenChrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe" );
+	        ChromeDriver driver = new ChromeDriver();
+	        driver.get("http://www.google.com/");
+	}
+
+}
