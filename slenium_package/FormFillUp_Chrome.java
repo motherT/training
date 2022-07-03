9e6b72847d3b746b83b101520578a8ff2992f897
ravi991 <raviranjancs4@gmail.com>
2022-07-03 15:48:56 -0400

    Update FormFillUp_Chrome.java

--- slenium_package/FormFillUp_Chrome.java
+++ slenium_package/FormFillUp_Chrome.java
@@ -19,40 +19,40 @@ public static void main(String[] args) {
     // sending text to the input box in the web form--------------
     // filling the name section
     driver.findElement(By.name("name")).sendKeys("DemoName");
-    System.out.println("name\")).sendKeys(\"DemoName");
+ 
     // E mail section
     driver.findElement(By.name("email")).sendKeys("DemoEmail@demo.com");
-    System.out.println("email\")).sendKeys(\"DemoEmail@demo.com");
+    
     // Website section
     driver.findElement(By.name("website")).sendKeys("www.demosite.com");
-    System.out.println("website\")).sendKeys(\"www.demosite.com");
+    
     // Comment section
     driver.findElement(By.name("comment")).sendKeys("demo comment");
-    System.out.println("comment\")).sendKeys(\"demo comment");
+    
     
     // Clicking radio button --------------------
     // Female Radio button select
     driver.findElement(By.xpath("//input[@value='female']")).click();
-    System.out.println("value='female']\")).click");
+  
     // male Radio button select
     driver.findElement(By.xpath("//input[@value='male']")).click();
-    System.out.println("value='male']\")).click");
+  
     // Other Radio button select
     driver.findElement(By.xpath("//input[@value='Other']")).click();
-    System.out.println("value='Other']\")).click");
+    
     // Clicking check-boxes What do you have: ---- Car is selected by default
     // Removing car option -- un-check
     driver.findElement(By.name("car")).click(); //-- this will un-check the car option as it is already selected
-    System.out.println("By.name(\"car\")).click--uncheck");
+  
     // Bike option check
     driver.findElement(By.name("bike")).click();
-    System.out.println("By.name(\"bike\")).click");
+    
     // boat option check
     driver.findElement(By.name("boat")).click();
-    System.out.println("By.name(\"boat\")).click");
+    
     // horse option check
     driver.findElement(By.name("horse")).click();
-    System.out.println("By.name(\"horse\")).click");
+    
     // Using Select option from selenium library
     // selecting option from menu web elements -- Country list select -- DropDown list
     //creating object for selecting web element
@@ -66,14 +66,14 @@ public static void main(String[] args) {
     Select skillSelect = new Select(driver.findElement(By.name("skill")));
     //using skillSelect to select menu options -- Programming
     skillSelect.selectByVisibleText("Programming");
-    System.out.println("selectByVisibleText(\"Programming");
+    
     //OR
     //using skillSelect to select menu options -- Database
     skillSelect.selectByVisibleText("Database");
-    System.out.println("selectByVisibleText(\"Database");
+    
     
     //Click on Send Button
     driver.findElement(By.name("submit")).click();
-    System.out.println("Form Fill-up Succesfull");
+   
 }
 }
