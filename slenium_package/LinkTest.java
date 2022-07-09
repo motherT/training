29d6cd61ae78e283867031e773c2c6064ee33c09
ravi991 <raviranjancs4@gmail.com>
2022-07-09 12:21:56 -0400

    Add files via upload

---
+++ slenium_package/LinkTest.java
@@ -0,0 +1,30 @@
+package seleniumPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LinkTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver;
+        // for running in Chrome, uncomment 2 following lines
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Administrator\\Documents\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+//        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Administrator\\Documents\\drivers\\geckodriver.exe");
+//        driver = new FirefoxDriver();
+        
+        String urlLink = "https://www.google.com/";
+        driver.get(urlLink);
+        WebElement blogLink = driver.findElement(By.linkText("Gmail"));
+        if (blogLink.isDisplayed()) {
+            System.out.println("Gmail link is diplayed usingLinkText");
+        } else {
+            System.out.println("Unable to find Gmail");
+        }
+        driver.close();
+	}
+
+}
