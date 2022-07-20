76f5ef9d6bac2a99469c903d1db07ef2a67894f3
ravi991 <raviranjancs4@gmail.com>
2022-07-20 14:55:19 -0400

    Update ActionExample.java

--- slenium_package/ActionExample.java
+++ slenium_package/ActionExample.java
@@ -7,14 +7,16 @@ import org.openqa.selenium.interactions.Actions;
 
 public class ActionExample {
 	public static void main(String[] args) {
+		// set the path of driver 
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+		// Create the object of driver.
 		ChromeDriver driver = new ChromeDriver();
-		
+		// pass the link of website
 		driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
         
         // Action class - action object -- passing driver information as parameter
         Actions action = new Actions(driver);    
-//        By locator = By.xpath("//span[@class='context-menu-one btn btn-neutral']");
+	// Locate the button locator 
         WebElement rightClickElement = driver.findElement(By.xpath("//span[@class='context-menu-one btn btn-neutral']"));
         
         // contextClick is the method() of class Action
