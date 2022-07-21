f263bf9024623a3a3ca1ad3c66574b7edd7c8bc7
ravi991 <raviranjancs4@gmail.com>
2022-07-21 13:35:24 -0400

    Update OpenChrome.java

--- slenium_package/OpenChrome.java
+++ slenium_package/OpenChrome.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class OpenChrome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// setProperty method is used to set the path of driver from system location 
 		  System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe" );
+		// Create the object of the driver 
 	        ChromeDriver driver = new ChromeDriver();
+		// Using driver object call the get method used for passing link.
 	        driver.get("http://www.google.com/");
 	}
 
