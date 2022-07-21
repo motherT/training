b3d334e12201a60572addf84dd9ca556f70746e2
ravi991 <raviranjancs4@gmail.com>
2022-07-21 13:38:56 -0400

    Update OpenCloseAmazon.java

--- slenium_package/OpenCloseAmazon.java
+++ slenium_package/OpenCloseAmazon.java
@@ -5,11 +5,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class OpenCloseAmazon {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe" );
+		
+	// setProperty method is used to set the path of driver from system location
+	System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe" );
+	// Create the object of the driver
         ChromeDriver driver = new ChromeDriver();
+	// Using driver object call the get method used for passing link.
         driver.get("http://www.amazon.com/");
+	// close method is used to close the driver 
         driver.close();
 	}
 
