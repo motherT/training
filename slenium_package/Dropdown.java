ecf6e537f1ada51cfdc9a659fc586dd3b01476a3
ravi991 <raviranjancs4@gmail.com>
2022-07-21 16:19:20 -0400

    Update Dropdown.java

--- slenium_package/Dropdown.java
+++ slenium_package/Dropdown.java
@@ -5,21 +5,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+// In this code we are trying to perform click on dropdown in form
+// In case of Dropdown we use Select class
+// Select the elements and make text visible and submit
 public class Dropdown {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver1.exe");
-        WebDriver driver = new ChromeDriver();
+		// setProperty method is used to set the path of driver from system location
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+		
+		// Intialize value to the driver
+        	WebDriver driver = new ChromeDriver();
+		// Using driver object call the get method and pass the website link
 	        driver.get("http://training.qaonlinetraining.com/testPage.php");
+		
+		// Use the select class in case of dropdown and locate the dropdown element and pass the locator in constructor
 	        Select countrySelect = new Select(driver.findElement(By.name("country")));
 	        
+		// using the select object call selectByVisibleText method and select the text in dropdown
 	        countrySelect.selectByVisibleText("USA"); //options [USA,India, Ethiopia,France]
 	        //or
 	        countrySelect.selectByVisibleText("France");
 	        
-	        //Select Option for skill menu
+	        //Same explanation for skill dropdown
 	        Select skillSelect = new Select(driver.findElement(By.name("skill")));
 	        //using skillSelect to select menu options -- Programming
 	        skillSelect.selectByVisibleText("Programming");
