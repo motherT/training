abd684f36bfd3edffab024350b6131f30de18665
ravi991 <raviranjancs4@gmail.com>
2022-07-22 08:15:23 -0400

    Update ArrayProgram.java

--- ArrayProgram.java
+++ ArrayProgram.java
@@ -6,7 +6,7 @@ public class ArrayProgram {
 		// //intialize the value in variable in collection of similar data types
 		int [] salary= {1000,2000,3000,4000,5000,6000,7000,8000,9000,10000};
 		
-		//to extract value one by one will use for loop and fetch the value at each location
+		//to extract value one by one use for loop and fetch the value at each indexes
 		for(int i =0;i< salary.length;i++)
 		{
 			System.out.println("Salary amount "+salary[i]);
