c28c73917a7e807ba09a73ec627a3e4ba7841b05
ravi991 <raviranjancs4@gmail.com>
2022-07-22 08:12:01 -0400

    Update ArrayProgram.java

--- ArrayProgram.java
+++ ArrayProgram.java
@@ -3,8 +3,10 @@ package javaPackage;
 public class ArrayProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// //intialize the value in variable in collection of similar data types
 		int [] salary= {1000,2000,3000,4000,5000,6000,7000,8000,9000,10000};
+		
+		//to extract value one by one will use for loop and fetch the value at each location
 		for(int i =0;i< salary.length;i++)
 		{
 			System.out.println("Salary amount "+salary[i]);
