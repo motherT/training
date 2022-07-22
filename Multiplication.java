75663c896658e72152818f0c9cff09f1238679a5
ravi991 <raviranjancs4@gmail.com>
2022-07-22 08:34:20 -0400

    Update Multiplication.java

--- Multiplication.java
+++ Multiplication.java
@@ -3,10 +3,12 @@ package javaPackage;
 public class Multiplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Intialize value to the variable 
 		int a =50;
 		int b=10;
+		// Print the mutiple of the variable 
 		int multi =a*b;
+		// Print the mutiple value
 		System.out.println("Multiplication "+multi);
 		
 	}
