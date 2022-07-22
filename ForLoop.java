a8cd53f9100d893c106c8a60943877baf472e07f
ravi991 <raviranjancs4@gmail.com>
2022-07-22 08:46:10 -0400

    Update ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -6,6 +6,7 @@ public class ForLoop {
 		// TODO Auto-generated method stub
 		for(int i =1;i<=10;i++)
 		{
+			//print the value in loop for 10 times till the condition gets false in loop
 			System.out.println("Name");
 		}
 	}
