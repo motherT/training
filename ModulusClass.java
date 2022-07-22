4cdbd8bf1547bff3cf16aedc1de16d23ed57e1ad
ravi991 <raviranjancs4@gmail.com>
2022-07-22 08:38:42 -0400

    Update ModulusClass.java

--- ModulusClass.java
+++ ModulusClass.java
@@ -6,6 +6,7 @@ public class ModulusClass {
 		// TODO Auto-generated method stub
 		int a =50;
 		int b=10;
+		// Modulus sign will give reminder after division that means if we divide 50/10 it will give reminder 0
 		int mod=a%b;
 		System.out.println("Print the reminder "+mod);
 	}
