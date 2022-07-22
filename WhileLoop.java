afb50ee7167d754b42261a20d24f0c0d6739e2e6
ravi991 <raviranjancs4@gmail.com>
2022-07-22 08:47:02 -0400

    Update WhileLoop.java

--- WhileLoop.java
+++ WhileLoop.java
@@ -5,9 +5,11 @@ public class WhileLoop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int count =1;
+		// perform the loop operation using while condition
 		while(count<=10)
 		{
 			System.out.println("Name");
+			// Increase the value by one .The loop works till the condition is false 
 			count++;
 		}
 	}
