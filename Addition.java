559665aebac9ae5157d751cf4931aa5a7fb3c34f
ravi991 <raviranjancs4@gmail.com>
2022-07-22 08:29:12 -0400

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -3,7 +3,7 @@ package javaPackage;
 public class Addition {
 
 	public static void main(String[] args) {
-		// Intialize value in two variable and add the variable to get sum
+		// Intialize value to variable and add the variable to get sum
 
 		int a =50;
 		int b =100;
