e7d94e855a3d1d1a032ff33a812d7595675e8e1c
ravi991 <raviranjancs4@gmail.com>
2022-07-22 08:35:59 -0400

    Update Division.java

--- Division.java
+++ Division.java
@@ -3,10 +3,12 @@ package javaPackage;
 public class Division {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Initialize the first variable
 		int a =50;
 		int b =10;
+		// divide the number 
 		int div =a/b;
+		// print the divide value
 		System.out.println("Division "+div);
 	}
 
