284fbe7d381cacc4ce0d13eab02d816fc53a87ad
ravi991 <raviranjancs4@gmail.com>
2022-07-22 08:33:12 -0400

    Update StringName.java

--- StringName.java
+++ StringName.java
@@ -3,8 +3,10 @@ package javaPackage;
 public class StringName {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Intialize a string value to a variable 
 		String name ="Name";
+		
+		// print the string value using the print statement
 		System.out.println("Print the name "+name);
 	}
 
