0afb3785545d8a7594bf4705197fa48c9f3e435d
ravi991 <raviranjancs4@gmail.com>
2022-07-22 08:21:05 -0400

    Update MultipleLines.java

--- MultipleLines.java
+++ MultipleLines.java
@@ -1,9 +1,10 @@
 package javaPackage;
 
+
 public class MultipleLines {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Print method used to print multiple output in console window
 		System.out.println("I am learning Java");
 		System.out.println("I am happy");
 		System.out.println("I am enjoying");
