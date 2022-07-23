b8c887e296e47d4ff2c6cf7108026d3c8cbb03ee
ravi991 <raviranjancs4@gmail.com>
2022-07-23 10:06:47 -0400

    Update SocialMedia.java

--- slenium_package/SocialMedia.java
+++ slenium_package/SocialMedia.java
@@ -6,10 +6,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SocialMedia {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// path of driver
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver1.exe");
         ChromeDriver driver=new ChromeDriver();
         driver.get("https://www.itlearn360.com/");
+	// Locate the icons on webpage using className locator and perform click and we can navigate to different icons using navigate method 
         driver.findElement(By.className("fa-facebook")).click();
         driver.navigate().back();
         driver.findElement(By.className("fa-twitter")).click();
