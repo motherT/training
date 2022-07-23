5915026d54314452877eaea81e6547ef7442182c
ravi991 <raviranjancs4@gmail.com>
2022-07-23 09:57:25 -0400

    Update ImplicitWait.java

--- slenium_package/ImplicitWait.java
+++ slenium_package/ImplicitWait.java
@@ -11,29 +11,24 @@ public class ImplicitWait {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver1.exe");
         ChromeDriver driver = new ChromeDriver();
-//        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Administrator\\Documents\\drivers\\geckodriver.exe");
-//        driver = new FirefoxDriver();
-        
-//        The Implicit Wait in Selenium is used to tell the web driver to wait for a certain amount of time before it throws a "No Such Element Exception". The default setting is 0. 
+	driver.get("https://www.todaytix.com/");
+//        The Implicit Wait in Selenium is used to tell the web driver to wait for a certain amount of time before it throws a "No Such Element Exception". 
+// 	The default setting is 0. 
 //        Once we set the time, the web driver will wait for the element for that time before throwing an exception
-        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
-        String SiteTitle = "Google";
-        // launch Chrome and redirect it to the Base URL
-        driver.get("http://google.com" );
-        //Maximizes the browser window
-        driver.manage().window().maximize() ;
-        //get the actual value of the title
-        String getTitle = driver.getTitle();
-        //compare the actual title with the expected title
-        if (getTitle.equals(SiteTitle))
-        {
-        System.out.println( "Test Passed") ;
-        }
-        else {
-        System.out.println( "Test Failed" );
-        }
-        //close browser
-        driver.close();
+	driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
+
+ 	// Locate all elements in webpage and click the signIn button
+     driver.findElement(By.xpath("//*[@id=\"navBarSignUpButton\"]/span[1]")).click();
+     driver.findElement(By.xpath("//span[text()='Email']")).click();
+     driver.findElement(By.name("firstName")).sendKeys("firstname");
+     driver.findElement(By.name("lastName")).sendKeys("lastName");
+
+// In case we inspect the email element we see the value of attribute keep changing everytime. Here we need implicit wait to locate element
+     driver.findElement(By.name("email")).sendKeys("info@gmail.com");
+     driver.findElement(By.name("confirmEmail")).sendKeys("info@gmail.com");
+     driver.findElement(By.name("password")).sendKeys("password"); 
+     driver.findElement(By.id("sign-up-submit")).click();
+     driver.close();
 
 	}
 
