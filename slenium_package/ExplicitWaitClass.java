094ca2bb3f6e4fc055b190464fda1ea92a77cdbc
ravi991 <raviranjancs4@gmail.com>
2022-07-24 14:09:34 -0400

    Update ExplicitWaitClass.java

--- slenium_package/ExplicitWaitClass.java
+++ slenium_package/ExplicitWaitClass.java
@@ -10,7 +10,7 @@ public class ExplicitWaitClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver1.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
         
       
