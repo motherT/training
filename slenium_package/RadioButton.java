5083a6f88a63b982873b73ae392cd85372568663
ravi991 <raviranjancs4@gmail.com>
2022-07-24 10:42:16 -0400

    Update RadioButton.java

--- slenium_package/RadioButton.java
+++ slenium_package/RadioButton.java
@@ -13,25 +13,24 @@ public class RadioButton {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		// setProperty method is used to set the path of driver from system location
+		// Set the path of chromeDriver browser
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 		
-		// Intialize value to the driver
+		// Intialize value to the chromeDriver browser
        		 WebDriver driver = new ChromeDriver();
-		
-		// Using driver object call the get method and pass the website link
+		// pass the link of website 
 	        driver.get("http://training.qaonlinetraining.com/testPage.php");
 	        // now we will save the locator of element in WebElement object .
 	        //radio option female
 	        WebElement radioButtonFemale = driver.findElement(By.xpath("//input[@value='female']"));
-//	        radio option male
+		// radio option male
 	        WebElement radioButtonMale = driver.findElement(By.xpath("//input[@value='male']"));
-//	        radio option Other
+		// radio option Other
 	        WebElement radioButtonOther = driver.findElement(By.xpath("//input[@value='Other']"));
 	        // submit button
 	        WebElement submitButton = driver.findElement(By.xpath("//input[@value='Send']"));
 		
-		// perform click on the elements in webpage
+		// perform click on the element in webpage
 	        radioButtonFemale.click();
 		
 		//Thread is a class in java used to hold a part of code for some given time using sleep method. 
@@ -42,4 +41,4 @@ public class RadioButton {
 
 }
 
-Note - WebElement is only used for saving the locators only.
+Note - WebElement is only used for saving the locators value.
