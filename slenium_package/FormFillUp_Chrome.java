d649a83d16ebbf8132e19439e964000f48066942
ravi991 <raviranjancs4@gmail.com>
2022-07-26 14:51:14 -0400

    Update FormFillUp_Chrome.java

--- slenium_package/FormFillUp_Chrome.java
+++ slenium_package/FormFillUp_Chrome.java
@@ -14,30 +14,30 @@ public class FormFillUp_Chrome {
 public static void main(String[] args) {
 	
     // In WebDriver we can intialize value of all drivers.So we can say WebDriver is universal class inside which all browser are defined.
-    WebDriver driver;
+    WebDriver browserDriver;
   // Set the path of chromeDriver browser
     System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 	
     // Create the object of the chromeBrowser driver
-    driver = new ChromeDriver();
+    browserDriver = new ChromeDriver();
     // opening website for form fill up
-    driver.get("http://www.training.qaonlinetraining.com/testPage.php");
+    browserDriver.get("http://www.training.qaonlinetraining.com/testPage.php");
     
     // sending text to the input box in the web form--------------
     // Locate the element using name locator  and pass  the value to the name section using sendKeys method
-    driver.findElement(By.name("name")).sendKeys("DemoName");
+    browserDriver.findElement(By.name("name")).sendKeys("DemoName");
  
     //Locate the element using name locator  and pass  the value to  E mail section using sendKeys method 
-    driver.findElement(By.name("email")).sendKeys("DemoEmail@demo.com");
+    browserDriver.findElement(By.name("email")).sendKeys("DemoEmail@demo.com");
     
     //Locate the element using name locator  and pass  the value to  Website section using sendKeys method
-    driver.findElement(By.name("website")).sendKeys("www.demosite.com");
+    browserDriver.findElement(By.name("website")).sendKeys("www.demosite.com");
     
     ///Locate the element using name locator  and pass  the value to  Comment section using sendKeys method
-    driver.findElement(By.name("comment")).sendKeys("demo comment");
+    browserDriver.findElement(By.name("comment")).sendKeys("demo comment");
     
    ///Locate the element using name locator  and perform click on submit button
-    driver.findElement(By.name("submit")).click();
+    browserDriver.findElement(By.name("submit")).click();
    
 }
 }
