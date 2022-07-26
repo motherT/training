2da6ad59cf90493ea46d6fef6ace28eb91be7d30
ravi991 <raviranjancs4@gmail.com>
2022-07-26 15:48:08 -0400

    Update DependencyClass.java

--- testngPackage/DependencyClass.java
+++ testngPackage/DependencyClass.java
@@ -5,40 +5,40 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class DependencyClass {
-	WebDriver driver;
+	WebDriver browserDriver;
 	@Test
     public void openingAmazonSite() throws InterruptedException {
         // for running in Chrome, uncomment 2 following lines
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
-        driver = new ChromeDriver();
+        browserDriver = new ChromeDriver();
 //        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Administrator\\Documents\\drivers\\geckodriver.exe");
 //        driver = new FirefoxDriver();
         System.out.println("Open /close  amazon.com in chrome browser");
-        driver.get("http://www.amazon.com/");
+        browserDriver.get("http://www.amazon.com/");
         Thread.sleep(2000);
-        driver.close();
+        browserDriver.close();
     }
     @Test(dependsOnMethods ="openingAmazonSite")
     public void openingCNNSite() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
-        driver = new ChromeDriver();
+        browserDriver = new ChromeDriver();
 //        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Administrator\\Documents\\drivers\\geckodriver.exe");
 //        driver = new FirefoxDriver();
         System.out.println("Open /close  cnn.com in chrome browser");
-        driver.get("http://www.cnn.com/");
+        browserDriver.get("http://www.cnn.com/");
         Thread.sleep(2000);
-        driver.close();
+        browserDriver.close();
     }
     @Test(dependsOnMethods ="openingCNNSite")
     public void openingYahooSite() throws InterruptedException {
         
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
-        driver = new ChromeDriver();
+        browserDriver = new ChromeDriver();
 //        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Administrator\\Documents\\drivers\\geckodriver.exe");
-//        driver = new FirefoxDriver();
+//        browserDriver = new FirefoxDriver();
         System.out.println("Open /close  yahoo.com in chrome browser");
-        driver.get("http://www.yahoo.com/");
+        browserDriver.get("http://www.yahoo.com/");
         Thread.sleep(2000);
-        driver.close();
+        browserDriver.close();
     }
 }
