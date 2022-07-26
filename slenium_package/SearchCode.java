7a631eeac2a6adb650308dd766f090f672d82811
ravi991 <raviranjancs4@gmail.com>
2022-07-26 15:36:55 -0400

    Update SearchCode.java

--- slenium_package/SearchCode.java
+++ slenium_package/SearchCode.java
@@ -8,12 +8,12 @@ public class SearchCode {
 	public static void main(String[] args) {
 		// set the path of driver 
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
+		ChromeDriver browserDriver = new ChromeDriver();
 		driver.get("https://www.google.com/");
 		
 		// pass the value to search in search button
-		driver.findElement(By.name("q")).sendKeys("Selenium python");
-		driver.findElement(By.name("btnK")).submit();
+		browserDriver.findElement(By.name("q")).sendKeys("Selenium python");
+		browserDriver.findElement(By.name("btnK")).submit();
 	}
 
 }
