f566a5da0a8ba6dbf67102f6e4c5e2c3d8828a79
ravi991 <raviranjancs4@gmail.com>
2022-07-26 15:38:52 -0400

    Update SocialMedia.java

--- slenium_package/SocialMedia.java
+++ slenium_package/SocialMedia.java
@@ -8,21 +8,21 @@ public class SocialMedia {
 	public static void main(String[] args) {
 		// path of driver
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver1.exe");
-        ChromeDriver driver=new ChromeDriver();
+        ChromeDriver browserDriver=new ChromeDriver();
         driver.get("https://www.itlearn360.com/");
 	// Locate the icons on webpage using className locator and perform click and we can navigate to different icons using navigate method 
-        driver.findElement(By.className("fa-facebook")).click();
-        driver.navigate().back();
-        driver.findElement(By.className("fa-twitter")).click();
-        driver.navigate().back();
-        driver.findElement(By.className("fa-instagram")).click();
-        driver.navigate().back();
-        driver.findElement(By.className("fa-youtube")).click();
-        driver.navigate().back();
-        driver.findElement(By.className("fa-linkedin")).click();
-        driver.navigate().back();
+        browserDriver.findElement(By.className("fa-facebook")).click();
+        browserDriver.navigate().back();
+        browserDriver.findElement(By.className("fa-twitter")).click();
+        browserDriver.navigate().back();
+        browserDriver.findElement(By.className("fa-instagram")).click();
+        browserDriver.navigate().back();
+        browserDriver.findElement(By.className("fa-youtube")).click();
+        browserDriver.navigate().back();
+        browserDriver.findElement(By.className("fa-linkedin")).click();
+        browserDriver.navigate().back();
         
-        driver.close();
+        browserDriver.close();
 
 	}
 
