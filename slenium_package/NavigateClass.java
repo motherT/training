be577e996bbb9e42bfca4680df6d2c6fbb230fff
ravi991 <raviranjancs4@gmail.com>
2022-07-26 15:39:44 -0400

    Update NavigateClass.java

--- slenium_package/NavigateClass.java
+++ slenium_package/NavigateClass.java
@@ -11,30 +11,30 @@ public class NavigateClass {
 		// set path of driver
 		
 	        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver1.exe");
-	      WebDriver  driver = new ChromeDriver();
-	        driver.get("https://www.facebook.com/");
+	      WebDriver  browserDriver = new ChromeDriver();
+	        browserDriver.get("https://www.facebook.com/");
 		// getTitle method will give me title in webpage
 	        System.out.println("this is " + driver.getTitle() + " website");
 		// getCurrentUrl method will give me url in webpage
 	        System.out.println("url is: " + driver.getCurrentUrl());
 	        // navigating to another site
-	        driver.navigate().to("http://www.instagram.com/");
+	        browserDriver.navigate().to("http://www.instagram.com/");
 	        System.out.println("this is " + driver.getTitle() + " website");
 	        System.out.println("url is: " + driver.getCurrentUrl());
 	        // navigating to another site
-	        driver.navigate().to("http://www.twitter.com/");
+	        browserDriver.navigate().to("http://www.twitter.com/");
 	        System.out.println("this is " + driver.getTitle() + " website");
 	        System.out.println("url is: " + driver.getCurrentUrl());
 	        // navigating back to gmail .com
-	        driver.navigate().back();
+	        browserDriver.navigate().back();
 	        System.out.println("this is " + driver.getTitle() + " website");
 	        System.out.println("url is: " + driver.getCurrentUrl());
 	        // navigating back to google .com
-	        driver.navigate().back();
+	        browserDriver.navigate().back();
 	        driver.navigate().forward();
 	        System.out.println("this is " + driver.getTitle() + " website");
 	        System.out.println("url is: " + driver.getCurrentUrl());
-	        driver.close();
+	        browserDriver.close();
 
 	}
 
