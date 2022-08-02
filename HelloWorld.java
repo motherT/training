a0fbac4a00e9a40ecec07d6413d11161b9fb0284
ravi991 <raviranjancs4@gmail.com>
2022-08-02 08:06:35 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ package javaPackage;
 
 public class HelloWorld {
 
+	// In this code we are trying to print output in console window using print statement 
+	
 	public static void main(String[] args) {
 		// Print method used to print the output in console window
 		System.out.println("Hello World");
