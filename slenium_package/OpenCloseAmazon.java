9c78b4060e872c4569bb83367ebceae5a175e112
ravi991 <raviranjancs4@gmail.com>
2022-08-02 15:37:43 -0400

    Update OpenCloseAmazon.java

--- slenium_package/OpenCloseAmazon.java
+++ slenium_package/OpenCloseAmazon.java
@@ -9,9 +9,9 @@ public class OpenCloseAmazon {
 		
 	// Set the path of driver 
 	System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe" );
-	// Create the object of the chromedriver browser 
+	// Create the reference of the chromedriver browser 
         ChromeDriver browserDriver = new ChromeDriver();
-	// pass the link of website in get method 
+	// pass the link of website in get method using the reference
         browserDriver.get("http://www.amazon.com/");
 	// close method is used to close the chromedriver browser
         browserDriver.close();
