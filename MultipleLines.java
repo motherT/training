57aeb34e0671e867e2df108e981c45f92a549b2c
ravi991 <raviranjancs4@gmail.com>
2022-08-02 08:12:03 -0400

    Update MultipleLines.java

--- MultipleLines.java
+++ MultipleLines.java
@@ -3,6 +3,7 @@ package javaPackage;
 
 public class MultipleLines {
 
+	// In this code we are trying to print mutiple lines of code in console window using print statement 
 	public static void main(String[] args) {
 		// Print method used to print multiple output in console window
 		System.out.println("I am learning Java");
