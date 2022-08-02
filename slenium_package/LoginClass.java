fee8f5544b8686c82f5f9024708be47a0b823b1f
ravi991 <raviranjancs4@gmail.com>
2022-08-02 16:42:21 -0400

    Update LoginClass.java

--- slenium_package/LoginClass.java
+++ slenium_package/LoginClass.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 // In this code we are trying to perform login to a website.
-// Locate the email and password input box 
-// clear the section as there is value by default and then send the value using sendkeys
-//Click the login button and submit the value
+// Here we will locate the email and pasword input box section 
+// In this case the email and pasword section is filled by default .So we need to clear this section. 
+// Once all section are filled click the submit button
 public class LoginClass {
 	
 	public static void main(String[] args) {
@@ -16,22 +16,22 @@ public class LoginClass {
 	WebDriver browserDriver;
 	// Set the path of chromeDriver browser
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
-	// Intialize value to the chromebrowser driver
+	// Create the reference of the chromedriver browser
         browserDriver = new ChromeDriver();
-	// pass the link of driver 
+	// get method helps to load the web pages in the browser that we pass in a given parameter.
         browserDriver.get("https://admin-demo.nopcommerce.com/login");
        
-	// Locate the email text box and clear the text box
+	// Locate the email text box and clear the text box using clear method 
         WebElement email=browserDriver.findElement(By.id("Email"));
         email.clear();
-	// Locate the password text box and clear the text box
+	// Locate the password text box and clear the text box using clear method 
         WebElement password=browserDriver.findElement(By.id("Password"));
         password.clear();
 	// send values in email box
         email.sendKeys("admin@yourstore.com");
 	// send values in password box
         password.sendKeys("admin");
-	// Locate the signIn button 
+	// Locate the signIn button and perform click
         WebElement button=browserDriver.findElement(By.className("button-1"));
 	// submit the value
         button.submit();
