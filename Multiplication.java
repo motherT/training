f25e959689b7858da37e15661caf0560a3fc376e
ravi991 <raviranjancs4@gmail.com>
2022-08-02 08:49:08 -0400

    Update Multiplication.java

--- Multiplication.java
+++ Multiplication.java
@@ -1,12 +1,13 @@
 package javaPackage;
 
+// In this code we are trying to mutiple the numers and print the otput in console window 
 public class Multiplication {
 
 	public static void main(String[] args) {
 		// Intialize value to the variable 
 		int a =50;
 		int b=10;
-		// Print the mutiple of the variable 
+		// Multiple the variables and print 
 		int multi =a*b;
 		// Print the mutiple value
 		System.out.println("Multiplication "+multi);
