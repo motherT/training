f13f139cf38bc534ff3badd44d02ac0ecfcf87cc
ravi991 <raviranjancs4@gmail.com>
2022-08-02 08:42:21 -0400

    Update Substraction.java

--- Substraction.java
+++ Substraction.java
@@ -1,9 +1,11 @@
 package javaPackage;
 
+// In this code we are trying to substarct two numbers  
+
 public class Substraction {
 
 	public static void main(String[] args) {
-		// Intialize value to variable and substract variable
+		// Intialize value to variable and substract the number to print in console window 
 		int a= 500;
 		int b=100;
 		int sub = a-b;
