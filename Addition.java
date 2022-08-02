7bc2d52f8eedf4ca63bb3324eb75a58d58b8ca7f
ravi991 <raviranjancs4@gmail.com>
2022-08-02 08:28:04 -0400

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,9 +1,10 @@
 package javaPackage;
 
+// In this code we are trying to add two numbers and print the sum of two numbers in console window 
 public class Addition {
 
 	public static void main(String[] args) {
-		// Intialize value to variable and add variable to get sum
+		// Intialize value to two variable and add variable to get sum
 
 		int a =50;
 		int b =100;
