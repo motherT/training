1eb7cba40bc2e144443f242b6eb6d963a6edee7a
ravi991 <raviranjancs4@gmail.com>
2022-08-03 16:33:01 -0400

    Update FormFillUp_Chrome.java

--- slenium_package/FormFillUp_Chrome.java
+++ slenium_package/FormFillUp_Chrome.java
@@ -1,42 +1,48 @@
-package seleniumPackage;
+// In this selenium code ,we will pass value to input text box in form and perform click on send button
+// Steps to perform in form 
+//1. setup the property of chromedriver because we will perform testing of login process through chrome web browser.
+// 2. Initialize Webdriver object through ChromeDriver class.
+// 3. Open form page of http://www.training.qaonlinetraining.com/testPage.php
+// 4. Locate Name section using name locator and send values to the the section
+// 5. Locate email section using name locator and send values to the the section
+// 6. Locate website section using name locator and send values to the the section
+// 7. Locate comment section using name locator and send values to the the section
+// 8. Locate the submit button and perform click
 
+
+package seleniumPackage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-
-// In this code we are trying to pass value in input box and click the send button
-// Here we need to locate all input elements using name locators and send values to all input box 
-// send values using sendkeys method and click method to perform click on elements 
-
 public class FormFillUp_Chrome {
 	
 public static void main(String[] args) {
 	
     // In WebDriver we can intialize value of all drivers.WebDriver is interface that helps in implementing  different browser values.
     WebDriver browserDriver;
-  // Set the path of chromeDriver browser
+  //1. setup the property of chromedriver because we will perform testing of login process through chrome web browser.
     System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 	
-    // Create the reference of the chromedriver browser
+    // 2. Initialize Webdriver object through ChromeDriver class.
     browserDriver = new ChromeDriver();
-    // get method helps to load the web pages in the browser that we pass in a given parameter.
+    // 3. Open form page of http://www.training.qaonlinetraining.com/testPage.php
     browserDriver.get("http://www.training.qaonlinetraining.com/testPage.php");
     
     // sending text to the input box in the web form--------------
-    // Locate the element using name locator  and pass  the value to the name section using sendKeys method
+    // 4. Locate Name section using name locator and send values to the the section
     browserDriver.findElement(By.name("name")).sendKeys("DemoName");
  
-    //Locate the element using name locator  and pass  the value to  E mail section using sendKeys method 
+    // 5. Locate email section using name locator and send values to the the section
     browserDriver.findElement(By.name("email")).sendKeys("DemoEmail@demo.com");
     
-    //Locate the element using name locator  and pass  the value to  Website section using sendKeys method
+   // 6. Locate website section using name locator and send values to the the section
     browserDriver.findElement(By.name("website")).sendKeys("www.demosite.com");
     
-    ///Locate the element using name locator  and pass  the value to  Comment section using sendKeys method
+    // 7. Locate comment section using name locator and send values to the the section
     browserDriver.findElement(By.name("comment")).sendKeys("demo comment");
     
-   ///Locate the element using name locator  and perform click on submit button
+   // 8. Locate the submit button and perform click
     browserDriver.findElement(By.name("submit")).click();
    
 }
