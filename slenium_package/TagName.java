d2e3a0a8036a7fe53ad5fdfafa477bfc70b90c44
ravi991 <raviranjancs4@gmail.com>
2022-08-04 16:47:07 -0400

    Update TagName.java

--- slenium_package/TagName.java
+++ slenium_package/TagName.java
@@ -4,7 +4,10 @@
 //2. Initialize Webdriver object through ChromeDriver class.
 //3. Open website link https://www.itlearn360.com/.
 //4. Intialize value to variable to keep count of number of links in webpage 
-//5. List all the tagName in one place.
+//5. List all the tagName in one list.
+//6. Now fetch the value inside the tagname using getAttribute method.To fetch value in sequence we will use for loop 
+//7. Calculate number of links and print in all.
+//8. Closing the driver using close method.
 
 package slenium_package;
 
@@ -18,17 +21,19 @@ public class TagName {
 	public static void main(String[] args) {
 
 		 WebDriver browserDriver;
-	        // Set the path of chromeDriver browser
+	       //1. setup the property of chromedriver because we will perform testing login form through chrome web browser.
 	        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
-		// Create the reference of the chromedriver browser
+		//2. Initialize Webdriver object through ChromeDriver class.
 	        browserDriver = new ChromeDriver();
-		// get method helps to load the web pages in the browser that we pass in a given parameter.
+		//3. Open website link https://www.itlearn360.com/.
 	     	 browserDriver.get("https://www.itlearn360.com/");
-		// Intialize value to a variable 
+		//4. Intialize value to variable to keep count of number of links in webpage 
 	        int count = 0;
 	        
-	        // FindElements is used to get multiple elements 
+	        //5. List all the tagName in one list.
 	        List<WebElement> allLinks = browserDriver.findElements(By.tagName("a"));
+		
+		//6. Now fetch the value inside the tagname using getAttribute method.To fetch value in sequence we will use for loop 
 	        for (WebElement link : allLinks) {
 	            // print the each url 
 	            System.out.println(link.getAttribute("href"));
@@ -36,10 +41,10 @@ public class TagName {
 	            count ++;
 	            
 	        }
-		// Print the total numbers of links
+		//7. Calculate number of links and print in all.
 	        System.out.println("total number of links present: "+count);
 		
-		// close the driver
+		//8. Closing the driver using close method.
 	        browserDriver.close();
 	}
 
