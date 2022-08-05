f1e9c7b2bb63f765701b1898f379abf3cc1068fe
ravi991 <raviranjancs4@gmail.com>
2022-08-05 03:01:21 -0400

    Update Dropdown.java

--- slenium_package/Dropdown.java
+++ slenium_package/Dropdown.java
@@ -1,47 +1,51 @@
-package slenium_package;
+// In this selenium code ,we will select value from dropdown and send selected value.
+// Steps to perform in case of dropdown. 
+// 1. setup the property of chromedriver because we will perform testing of login process through chrome web browser.
+// 2. Initialize Webdriver object through ChromeDriver class.
+// 3. Open the form page http://training.qaonlinetraining.com/testPage.php
+// 4. Select class is used in case of dropdown to pass the dropdown element. 
+// 5. Using the reference of Select class call the selectByVisibleText method and select text in dropdown
+// 6. Locate the send button and perform click
+// 7. Close the web browser
 
+package slenium_package;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-// In this code we are trying to perform click on dropdown in form
-// In case of Dropdown we use Select class
-// Select the elements and make text visible and submit
 public class Dropdown {
 
 	public static void main(String[] args) {
-		// Set the path of chromeDriver browser
+		// 1. setup the property of chromedriver because we will perform testing of login process through chrome web browser.
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 		
-		// Intialize value to the chromeDriver browser
+		// 2. Initialize Webdriver object through ChromeDriver class.
         	WebDriver browserDriver = new ChromeDriver();
-		// pass the link of website
+		// 3. Open the form page http://training.qaonlinetraining.com/testPage.php
 	        browserDriver.get("http://training.qaonlinetraining.com/testPage.php");
 		
-		// Select class is used in case of dropdown and pass the value of element .
+		// 4. Select class is used in case of dropdown to pass the dropdown element. 
 	        Select countrySelect = new Select(browserDriver.findElement(By.name("country")));
 	        
-		// using the countrySelect call selectByVisibleText method and select the text in dropdown
+		// 5. Using the reference of Select class call the selectByVisibleText method and select text in dropdown
 	        countrySelect.selectByVisibleText("USA"); //options [USA,India, Ethiopia,France]
 	        //or
 	        countrySelect.selectByVisibleText("France");
 	        
-	        //Same explanation for skill dropdown
+	       // Same explanation for skill dropdown 
 	        Select skillSelect = new Select(browserDriver.findElement(By.name("skill")));
 	        //using skillSelect to select menu options -- Programming
 	        skillSelect.selectByVisibleText("Programming");
-	        System.out.println("selectByVisibleText(\"Programming");
-	        //OR
-	        //using skillSelect to select menu options -- Database
+	     
 	        skillSelect.selectByVisibleText("Database");
 	        System.out.println("selectByVisibleText(\"Database");
 	        
-	        //Click on Send Button
+	        // 6. Locate the send button and perform click
 	        browserDriver.findElement(By.name("submit")).click();
 	        System.out.println("Form Fill-up Succesfull");
-		// quit method close all the browser open in screen
-	        browserDriver.quit();
+		// 7. Close the web browser
+	        browserDriver.close();
 	}
 
 }
