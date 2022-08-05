16330bc58fb3753334f81442ee8f4eceb66d8235
ravi991 <raviranjancs4@gmail.com>
2022-08-05 09:11:19 -0400

    Update OpenChrome.java

--- slenium_package/OpenChrome.java
+++ slenium_package/OpenChrome.java
@@ -1,19 +1,20 @@
+// In this selenium code we are opening the website link using chromeDriver
+// Steps to perform 
+// 1. setup the property of chromedriver because we will perform testing of login process through chrome web browser.
+// 2. Initialize Webdriver object through ChromeDriver class.
+// 3. Open the http://www.google.com/ link using get method.
+
 package seleniumPackage;
-
 import org.openqa.selenium.chrome.ChromeDriver;
-
-// In this code we are trying open a google chrome website in chromedriver browser
-
-
 public class OpenChrome {
 
 	public static void main(String[] args) {
-		// Set the path of driver 
+		// 1. setup the property of chromedriver because we will perform testing of login process through chrome web browser.
 		  System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe" );
-		// Create the reference of the chromedriver browser 
-	        ChromeDriver browserDriver = new ChromeDriver();
-		// get method helps to load the web pages in the browser that we pass in a given parameter.
-	        browserDriver.get("http://www.google.com/");
+		// 2. Initialize Webdriver object through ChromeDriver class.
+	        WebDriver browserObject = new ChromeDriver();
+		// 3. Open the http://www.google.com/ link using get method.
+	        browserObject.get("http://www.google.com/");
 	}
 
 }
