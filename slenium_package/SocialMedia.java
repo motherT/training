5ffea7444316f0bb6a6368284a1ae4274e609d61
ravi991 <raviranjancs4@gmail.com>
2022-08-05 11:17:19 -0400

    Update SocialMedia.java

--- slenium_package/SocialMedia.java
+++ slenium_package/SocialMedia.java
@@ -25,31 +25,31 @@ public class SocialMedia {
 	// 1. setup the property of WebDriver to perform navigate on icons through chrome web browser.
 	System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 	// 2. Initialize WebDriver object through ChromeDriver class.
-        WebDriver browserDriver=new ChromeDriver();
+        WebDriver browserObject=new ChromeDriver();
 	// 3. Open the web page https://www.itlearn360.com/
-        driver.get("https://www.itlearn360.com/");
+        browserObject.get("https://www.itlearn360.com/");
 	// 4. Locate facebook icons using className locator and perform click
-        browserDriver.findElement(By.className("fa-facebook")).click();
+        browserObject.findElement(By.className("fa-facebook")).click();
 	// 5. Navigate back on main page using navigate and back method 
-        browserDriver.navigate().back();
+        browserObject.navigate().back();
 	// 6. Locate twitter icons using className locator and perform click
-        browserDriver.findElement(By.className("fa-twitter")).click();
+        browserObject.findElement(By.className("fa-twitter")).click();
 	// 7. Navigate back on main page using navigate and back method
-        browserDriver.navigate().back();
+        browserObject.navigate().back();
 	// 8. Locate instagram icons using className locator and perform click
-        browserDriver.findElement(By.className("fa-instagram")).click();
+        browserObject.findElement(By.className("fa-instagram")).click();
 	// 9. Navigate back on main page using navigate and back method
-        browserDriver.navigate().back();
+        browserObject.navigate().back();
 	// 10. Locate youtube icons using className locator and perform click
-        browserDriver.findElement(By.className("fa-youtube")).click();
+        browserObject.findElement(By.className("fa-youtube")).click();
 	// 11. Navigate back on main page using navigate and back method
-        browserDriver.navigate().back();
+        browserObject.navigate().back();
 	// 12. Locate linkedIn icons using className locator and perform click
-        browserDriver.findElement(By.className("fa-linkedin")).click();
+        browserObject.findElement(By.className("fa-linkedin")).click();
 	// 13. Navigate back on main page using navigate and back method
-        browserDriver.navigate().back();
+        browserObject.navigate().back();
         // 14. Close the browser
-        browserDriver.close();
+        browserObject.close();
 
 	}
 
