ed9c5b16f0b2c92e62676c6aca86f1c11893b329
ravi991 <raviranjancs4@gmail.com>
2022-08-05 04:08:20 -0400

    Update AlertExample.java

--- slenium_package/AlertExample.java
+++ slenium_package/AlertExample.java
@@ -1,54 +1,65 @@
-package slenium_package;
 
+// In this selenium code ,different types of alert .
+// Steps to perform in case of dropdown. 
+// 1. setup the property of chromedriver because we will perform testing of login process through chrome web browser.
+// 2. Initialize Webdriver object through ChromeDriver class.
+// 3. Open the form page http://training.qaonlinetraining.com/testPage.php
+// 4. Locate the alert button and perform click 
+// 5. Intialize Alert object using switchTo and alert method
+// 6. getText method is used to get the text from alert box 
+// 7. Thread.sleep is used to hold the part of code for given time limit. In this case take mouse over the thread and add Interrupted exception to this method
+// 8. Switch the browserdriver control to alert box and accept the alert 
+// 9. Switch the browserdriver control to alert box and cancel the alert 
+// 10. Switch the browserdriver control to alert box and send value to the alert box using sendkeys method
+// 11. Close the web browser
+
+package slenium_package;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-// In this code we are trying to see different types of alert avialable in webpage 
-// Test each alert button one by one and see the output 
-
-
 public class AlertExample {
 
 	public static void main(String[] args) throws InterruptedException {
-		// Set the path of chromeDriver browser
+		// 1. setup the property of chromedriver because we will perform testing of login process through chrome web browser.
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 		
-		// Intialize value to the chromeDriver browser
+		// 2. Initialize Webdriver object through ChromeDriver class.
 		ChromeDriver browserDriver = new ChromeDriver();
 		
-		// pass the link of website
+		// 3. Open the form page http://training.qaonlinetraining.com/testPage.php
 	        browserDriver.get("http://training.qaonlinetraining.com/testPage.php");
 	        
-		// Locate the alert button and perform click 
+		// 4. Locate the alert button and perform click 
 	        browserDriver.findElement(By.id("alert")).click();
-		// Use Alert class in case of alert and to switch from webpage to alert box call switchTo() and alert()
+		// 5. Intialize Alert object using sitchTo and alert method
 	        Alert alert = browserDriver.switchTo().alert();
 		
-		// to get the text from the alert box use getText()
-	        System.out.println(alert.getText());
+		// 6. getText method is used to get the text from alert box 
+	        System.out.println(alert.getText());\
+		// 7. Thread.sleep is used to hold the part of code for given time limit. In this case take mouse over the thread and add Interrupted exception to this method	
 	        Thread.sleep(6000);
 		
-		// to accept the alert use accept()
+		// 8. Switch the browserdriver control to alert box and accept the alert 
 	        alert.accept();
 	        
 	        // example 2 - dismiss(You pressed Cancel!)
-		//The example is same to 1st one here we cancel the alert so we use dismiss()
-	        browserDriver.findElement(By.id("confirm")).click();
+		browserDriver.findElement(By.id("confirm")).click();
 	        Alert alert1 = browserDriver.switchTo().alert();
 	        System.out.println(alert1.getText());
 	        Thread.sleep(6000);
+		// 9. Switch the browserdriver control to alert box and cancel the alert 
 	        alert1.dismiss();
 	        
-	        // example 3 - accept and Type(Hello Mr. Bond! How are you today?)
-		// In this example we are trying to send value to the input box inside alert 
+	        // example 3 - accept and Type(Hello Mr. Bond! How are you today?
 	        browserDriver.findElement(By.id("prompt")).click();
 	        Alert alert2 = browserDriver.switchTo().alert();
 	        System.out.println(alert2.getText());
 	        Thread.sleep(6000);
-		// Use sendkeys() to send value to input box in alert box
+		// 10. Switch the browserdriver control to alert box and send value to the alert box using sendkeys method
 	        alert2.sendKeys("Mr. Bond");
+		// 11. Close the web browser
 	        alert2.accept();
 	}
 
