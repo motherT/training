d8c7d8a2473755654d46c7f1f3992f5425dbb6f7
ravi991 <raviranjancs4@gmail.com>
2022-08-05 14:20:52 -0400

    Update LinkTest.java

--- slenium_package/LinkTest.java
+++ slenium_package/LinkTest.java
@@ -1,29 +1,41 @@
-package seleniumPackage;
+//In this Selenium code,we are trying to check the text is link or not.
+// Steps to perform 
+// 1. setup the property of WebDriver to perform linktext through chrome web browser.
+// 2. Initialize WebDriver object through ChromeDriver class.
+// 3. Open the web page https://www.google.com/
+// 4. Get the reference of the gmail text using textLink.
+// 5. Then check  the text is displayed  or not on webpage
+// 6. Close the browser
 
+package seleniumPackage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LinkTest {
-
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		
-		WebDriver browserDriver;
-        // set the path of driver 
+	WebDriver browserObject;
+       // 1. setup the property of WebDriver to perform linktext through chrome web browser.
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
-        browserDriver = new ChromeDriver();
-        
-        String urlLink = "https://www.google.com/";
-        browserDriver.get(urlLink);
-        WebElement blogLink = browserDriver.findElement(By.linkText("Gmail"));
-	// isDisplayed method is used to check whether the element is displayed on webpage
+	// 2. Initialize WebDriver object through ChromeDriver class.
+        browserObject = new ChromeDriver();
+       
+	// 3. Open the web page https://www.google.com/
+        browserObject.get(https://www.google.com/);
+	// 4. Get the reference of the gmail text using textLink.		  
+        WebElement blogLink = browserObject.findElement(By.linkText("Gmail"));
+			  
+			 
+	// 5. Then check  the text is displayed  or not on webpage
         if (blogLink.isDisplayed()) {
             System.out.println("Gmail link is diplayed usingLinkText");
         } else {
             System.out.println("Unable to find Gmail");
         }
-        browserDriver.close();
+	// 6. Close the browser
+        browserObject.close();
 	}
 
 }
