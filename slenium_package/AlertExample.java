296e8889bc342862430c5e9ee8651c30040732ee
ravi991 <raviranjancs4@gmail.com>
2022-08-05 10:36:54 -0400

    Update AlertExample.java

--- slenium_package/AlertExample.java
+++ slenium_package/AlertExample.java
@@ -8,9 +8,10 @@
 // 5. Intialize Alert object using switchTo and alert method
 // 6. getText method is used to get the text from alert box 
 // 7. Thread.sleep is used to hold the part of code for given time limit. In this case take mouse over the thread and add Interrupted exception to this method
-// 8. Switch the browserdriver control to alert box and accept the alert 
-// 9. Switch the browserdriver control to alert box and cancel the alert 
+// 8. Switch the browserdriver control to alert box and accept the alert uing accept method
+// 9. Switch the browserdriver control to alert box and cancel the alert using dismiss method 
 // 10. Switch the browserdriver control to alert box and send value to the alert box using sendkeys method
+// 11. After the value is send accept the alert using accept method.
 // 11. Close the web browser
 
 package slenium_package;
@@ -26,41 +27,44 @@ public class AlertExample {
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 		
 		// 2. Initialize Webdriver object through ChromeDriver class.
-		ChromeDriver browserDriver = new ChromeDriver();
+		ChromeDriver browserObject = new ChromeDriver();
 		
 		// 3. Open the form page http://training.qaonlinetraining.com/testPage.php
-	        browserDriver.get("http://training.qaonlinetraining.com/testPage.php");
+	        browserObject.get("http://training.qaonlinetraining.com/testPage.php");
 	        
 		// 4. Locate the alert button and perform click 
-	        browserDriver.findElement(By.id("alert")).click();
+	        browserObject.findElement(By.id("alert")).click();
 		// 5. Intialize Alert object using sitchTo and alert method
-	        Alert alert = browserDriver.switchTo().alert();
+	        Alert alert = browserObject.switchTo().alert();
 		
 		// 6. getText method is used to get the text from alert box 
 	        System.out.println(alert.getText());\
 		// 7. Thread.sleep is used to hold the part of code for given time limit. In this case take mouse over the thread and add Interrupted exception to this method	
 	        Thread.sleep(6000);
 		
-		// 8. Switch the browserdriver control to alert box and accept the alert 
+		// 8. Switch the browserdriver control to alert box and accept the alert uing accept method 
 	        alert.accept();
 	        
 	        // example 2 - dismiss(You pressed Cancel!)
-		browserDriver.findElement(By.id("confirm")).click();
-	        Alert alert1 = browserDriver.switchTo().alert();
+		browserObject.findElement(By.id("confirm")).click();
+	        Alert alert1 = browserObject.switchTo().alert();
 	        System.out.println(alert1.getText());
 	        Thread.sleep(6000);
-		// 9. Switch the browserdriver control to alert box and cancel the alert 
+		// 9. Switch the browserdriver control to alert box and cancel the alert using dismiss method 
 	        alert1.dismiss();
 	        
 	        // example 3 - accept and Type(Hello Mr. Bond! How are you today?
-	        browserDriver.findElement(By.id("prompt")).click();
-	        Alert alert2 = browserDriver.switchTo().alert();
+	        browserObject.findElement(By.id("prompt")).click();
+	        Alert alert2 = browserObject.switchTo().alert();
 	        System.out.println(alert2.getText());
 	        Thread.sleep(6000);
 		// 10. Switch the browserdriver control to alert box and send value to the alert box using sendkeys method
 	        alert2.sendKeys("Mr. Bond");
-		// 11. Close the web browser
+		// 11. After the value is send accept the alert using accept method.
 	        alert2.accept();
+		
+		// 12 .Close the web browser
+		browserObject.close();
 	}
 
 }
