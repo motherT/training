f3c365dd8186eebf65cad7e2fda7bfae109adb38
ravi991 <raviranjancs4@gmail.com>
2022-08-06 12:30:26 -0400

    Update OpenCloseAmazon.java

--- slenium_package/OpenCloseAmazon.java
+++ slenium_package/OpenCloseAmazon.java
@@ -1,6 +1,6 @@
-// In this selenium code we are opening the website link using chromeDriver
+// In this selenium code we are opening amazon website link using chromeDriver
 // Steps to perform 
-// 1. setup the property of chromedriver because we will perform testing of login process through chrome web browser.
+// 1. setup the property of chromedriver to open amazon page through chrome web browser.
 // 2. Initialize Webdriver object through ChromeDriver class.
 // 3. Open the http://www.amazon.com/ link using get method.
 
@@ -8,7 +8,7 @@ package seleniumPackage;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class OpenCloseAmazon {
 public static void main(String[] args) {
-	// 1. setup the property of chromedriver because we will perform testing of login process through chrome web browser.
+	// 1. setup the property of chromedriver to open amazon page through chrome web browser.
 	System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe" );
 	// 2. Initialize Webdriver object through ChromeDriver class. 
         WebDriver browserObject = new ChromeDriver();
