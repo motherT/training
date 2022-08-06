1e08e2e8dcd2ac2c93de9f45741432a9a225e596
ravi991 <raviranjancs4@gmail.com>
2022-08-06 17:09:57 -0400

    Update ExplicitWaitClass.java

--- slenium_package/ExplicitWaitClass.java
+++ slenium_package/ExplicitWaitClass.java
@@ -1,16 +1,14 @@
-// In this code of selenium, perform implicit wait on webpage 
+// In this selenium code, explicit wait is applied to a element.
 // Steps to perform 
-// 1. setup the property of WebDriver to perform "implicit wait" through chrome web browser.
+// 1. setup the property of WebDriver to perform "Explicit Wait" through chrome web browser.
 // 2. Initialize WebDriver object through ChromeDriver class.
-// 3. Apply implict wait to webpage .implicitlyWait method  holds the webdriver for a certain amount of time before throwing "No Such Element Exception"
-// In this given time the wait will  locate the element. 
-// 4. Intialize the value of title.
-// 5. Open the web page https://www.google.com/
-// 6. maximaize the browser window
-// 7. Get the title of webpage 
-// 8. Compare the actual element and expected element 
-// 9. close the web browser
-package slenium_package;
+// 3. In Explicit Wait we will use WebDriverWait class .This class object is created and the the browserObject and time is intialized to the object
+// Explicit Wait is applied to element in the webpage.
+// 4.  Open the web page https://www.google.com/
+// 5. Locate the gmail element and apply the wait and until method to the element.
+// 6. close the browser
+
+package seleniumPackage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,36 +18,25 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class ExplicitWaitClass {
 
 	public static void main(String[] args) {
-	// 1. setup the property of WebDriver to perform "implicit wait" through chrome web browser.
+	// 1. setup the property of WebDriver to perform "Explicit Wait" through chrome web browser.
 	System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
-
 	// 2. Initialize WebDriver object through ChromeDriver class.
-	WebDriver browserObject = new ChromeDriver(); 
-	// 3. Apply implict wait to webpage .implicitlyWait method  holds the webdriver for a certain amount of time before throwing "No Such Element Exception".
-	// In this given time the wait will  locate the element.
-	browserObject.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
-	
-	// 4. Intialize the value of title.
-	String eTitle = "Google";
-
-	// 5. Open the web page https://www.google.com/
-	browserObject.get("https://www.google.com/");
-		
-	// 6. maximaize the browser window
-	browserObject.manage().window().maximize() ;
-	// 7. Get the title of webpage 
-	String aTitle = browserObject.getTitle();
-	// 8. Compare the actual element and expected element
-	if (aTitle.equals(eTitle))
-	{
-	System.out.println( "Test Passed") ;
-	}
-	else {
-	System.out.println( "Test Failed" );
-	}
-	// 9. close the web browser
-	browserObject.close();
-
+        WebDriver browserObject = new ChromeDriver();
+      
+	// 3. In Explicit Wait we will use WebDriverWait class .This class object is created and the the browserObject and time is intialized to the object
+	// Explicit Wait is applied to element in the webpage
+      	WebDriverWait wait=new WebDriverWait(browserObject, 10);
+     
+     // 4.  Open the web page https://www.google.com/
+      browserObject.get("http://google.com" );
+      //Maximizes the browser window
+     
+      WebElement gmailLink;
+	// 5. Locate the gmail element and apply the wait and until method to the element.
+      gmailLink= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath( "//a[contains(text(),'Gmaileeee')]")));
+      gmailLink.click();
+     // 6. close the browser
+   browserObject.quit();
 	}
 
 }
