8a392d66e5166b5bcf8a627e62530e3e060bb515
ravi991 <raviranjancs4@gmail.com>
2022-08-06 15:34:42 -0400

    Update ActionExample.java

--- slenium_package/ActionExample.java
+++ slenium_package/ActionExample.java
@@ -3,9 +3,9 @@
 // 1. setup the property of chromedriver because we will perform testing of Mouse Over operation through chrome web browser.
 // 2. Initialize WebDriver object through ChromeDriver class.
 // 3. Open the web page http://swisnl.github.io/jQuery-contextMenu/demo.html
-// 4. In case of rightclick we use Actions class.Create refernce and pass the driver to the reference
-// 5. Get the reference of button to perform right click.
-// 6. Using the action reference call the contextclick method and pass  element to perform right click
+// 4. In case of RightClick we use Actions class.Create object and pass the driver to the reference
+// 5. Locate the button using xpath and store the value in the reference that is rightClickElement
+// 6. Using the action object call the contextclick method and pass  element to perform right click
 
 package slenium_package;
 import org.openqa.selenium.By;
@@ -22,9 +22,9 @@ public class ActionExample {
 		// 3. Open the web page http://swisnl.github.io/jQuery-contextMenu/demo.html
 		browserObject.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
         
-        // 4. In case of RightClick we use Actions class.Create refernce and pass the driver to the reference
+        // 4. In case of RightClick we use Actions class.Create object and pass the driver to the reference
         Actions action = new Actions(browserObject);    
-	// 5. Get the reference of button to perform right click.
+	// 5. Locate the button using xpath and store the value in the reference that is rightClickElement
         WebElement rightClickElement = browserObject.findElement(By.xpath("//span[@class='context-menu-one btn btn-neutral']"));
         
        // 6. Using the action reference call the contextclick method and pass  element to perform right click
