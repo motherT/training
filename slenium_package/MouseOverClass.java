ba3f8c38fc0fa1a952f4b2f2b66e979da15cac4c
ravi991 <raviranjancs4@gmail.com>
2022-08-06 15:53:02 -0400

    Update MouseOverClass.java

--- slenium_package/MouseOverClass.java
+++ slenium_package/MouseOverClass.java
@@ -4,9 +4,9 @@
 // 2. Initialize WebDriver object through ChromeDriver class.
 // 3. Open the web page https://www.amazon.com/
 // 4. Thread.sleep is used to hold the part of code for given time limit. In this case take mouse over the thread and add Interrupted exception to this method
-// 5. Get the reference of element to mouse over.
-// 6. In case of mouse Over we use Actions class.Create refernce and pass the driver to the reference
-// 7. Using the action reference call the moveToElement method and pass  element to mouse over
+// 5. Locate the element using the xpath and store in the reference SiginInelement 
+ // 6. In case of RightClick we use Actions class.Create object and pass the driver to the reference
+// 7. Using the action object call the moveToElement method and pass  SigiInelement reference to this method and perform mouseOver
 // 8. Select a element from the pop up box and perform click
 // 9. Close the browser
 
@@ -26,29 +26,30 @@ public class MouseOverClass {
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 		
 		// 2. Initialize WebDriver object through ChromeDriver class.
-		WebDriver browserDriver = new ChromeDriver();
+		WebDriver browserObject = new ChromeDriver();
 		
 		// 3. Open the web page https://www.amazon.com/
-		 browserDriver.get("https://www.amazon.com/");
+		 browserObject.get("https://www.amazon.com/");
 		
 		// 4. Thread.sleep is used to hold the part of code for given time limit. In this case take mouse over the thread and add Interrupted exception to this method
 	        Thread.sleep(3000);
-		// 5. Get the reference of element to mouse over.
-	        WebElement element = browserDriver.findElement(By.xpath("//span[contains(text(),'Hello, Sign in')]")) ;
+		// 5. Locate the element using the xpath and store in the reference SiginInelement 
+	        WebElement element = browserObject.findElement(By.xpath("//span[contains(text(),'Hello, Sign in')]")) ;
 	        
 		
 	      
-		// 6. In case of Mouse Over we use Actions class.Create refernce and pass the driver to the reference
-	        Actions action = new Actions(browserDriver);
+		 // 6. In case of RightClick we use Actions class.Create object and pass the driver to the reference
+	        Actions action = new Actions(browserObject);
 	        System.out.println("Mouse is over the - HELLO, SIGN IN - option");
 	        
-	        // 7. Using the action reference call the moveToElement method and pass  element to mouse over
+	        // 7. Using the action object call the moveToElement method and pass  SigiInelement reference to this method and perform mouseOver
 	        action.moveToElement(element).build().perform();
 	        //Thread.sleep(3000);
 		// 8. Select a element from the pop up box and perform click
-	        browserDriver.findElement(By.xpath("//span[contains(text(),'Create a List')]")).click();
+	        browserObject.findElement(By.xpath("//span[contains(text(),'Create a List')]")).click();
 		// 9. Close the browser
-	        System.out.println("Clicked");
+		browserObject.close();
+	        
 
 	}
 
