f382700cfe13122269ecde804e2cff06a1fa8e40
ravi991 <raviranjancs4@gmail.com>
2022-08-06 12:51:18 -0400

    Update OpenChrome.java

--- slenium_package/OpenChrome.java
+++ slenium_package/OpenChrome.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class OpenChrome {
 
 	public static void main(String[] args) {
-		// 1. setup the property of chromedriver because we will perform testing of login process through chrome web browser.
+		// 1. setup the property of chromedriver to open google page through chrome web browser.
 		  System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe" );
 		// 2. Initialize Webdriver object through ChromeDriver class.
 	        WebDriver browserObject = new ChromeDriver();
