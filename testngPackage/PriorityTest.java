77fb83cec04ae7ecec0950066fcea86020966414
ravi991 <raviranjancs4@gmail.com>
2022-08-07 13:45:16 -0400

    Update PriorityTest.java

--- testngPackage/PriorityTest.java
+++ testngPackage/PriorityTest.java
@@ -30,14 +30,14 @@ public class PriorityTest {
     }
    // 1. The @Test is used along with priority and based on priority the method will exceute. The piority is set as 2 .So this will exceute second.
     @Test(priority =2)
-    public void openingCNNSite() throws InterruptedException {
+    public void openingCNNBCSite() throws InterruptedException {
 	 // 2. setup the property of chromedriver to perform mutiple test case through chrome web browser.
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 	 // 3. Initialize WebDriver object through ChromeDriver class.
         browserObject = new ChromeDriver();
         System.out.println("Open /close  cnn.com in chrome browser");
 	 // 4. Open the web page http://www.cnn.com/ in second test case
-        browserObject.get("http://www.cnn.com/");
+        browserObject.get("https://www.cnbc.com/");
 	 // 5. Thread.sleep is used to hold the part of code for given time limit. In this case take mouse over the thread and add Interrupted exception to this method
         Thread.sleep(2000);
 	 // 6. close the browser
