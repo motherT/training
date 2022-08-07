94001173a6bc212dc43d754057e580b2ba92d56a
ravi991 <raviranjancs4@gmail.com>
2022-08-07 13:46:23 -0400

    Update DependencyClass.java

--- testngPackage/DependencyClass.java
+++ testngPackage/DependencyClass.java
@@ -31,14 +31,14 @@ public class DependencyClass {
     }
    // 7. In next method to set the dependency use dependOMethods and pass the method name depended to.
     @Test(dependsOnMethods ="openingAmazonSite")
-    public void openingCNNSite() throws InterruptedException {
+    public void openingCNBCSite() throws InterruptedException {
 	// 2. setup the property of WebDriver to set the dependency of method through chrome web browser
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 	// 3. Initialize WebDriver object through ChromeDriver class.
         browerObject = new ChromeDriver();
         System.out.println("Open /close  cnn.com in chrome browser");
 	// 4. Open the web page http://www.cnn.com/ in first test case 
-        browerObject.get("http://www.cnn.com/");
+        browerObject.get("https://www.cnbc.com/");
 	// 5. Thread.sleep is used to hold the part of code for given time limit. In this case take mouse over the thread and add Interrupted exception to this method
         Thread.sleep(2000);
 	 // 6. close the browser
