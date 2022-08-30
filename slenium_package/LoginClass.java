2b5205f8ce6acc99fe236ceb2e472eeda6f1a5b5
ravi991 <raviranjancs4@gmail.com>
2022-08-30 13:49:54 -0400

    Update LoginClass.java

--- slenium_package/LoginClass.java
+++ slenium_package/LoginClass.java
@@ -31,11 +31,11 @@ public class LoginClass {
         browserObject.get("https://admin-demo.nopcommerce.com/login");
        
 	// 4. Get reference of Email input box by locate the id method.
-        WebElement email=browserDriver.findElement(By.id("Email"));
+        WebElement email=browserObject.findElement(By.id("Email"));
 	// 5. Clear the Email input box.
         email.clear();
 	// 6. Get reference of Password input box by locate the id method.
-        WebElement password=browserDriver.findElement(By.id("Password"));
+        WebElement password=browserObject.findElement(By.id("Password"));
 	// 7. Clear the Password input box.
         password.clear();
 	// 8. Provide/Enter the correct email to email input box through reference.
@@ -43,7 +43,7 @@ public class LoginClass {
 	// 9. Provide/Enter the correct password to password input box through reference.
         password.sendKeys("admin");
 	// 10. Get reference of login button by locate the classname method.
-        WebElement button=browserDriver.findElement(By.className("button-1"));
+        WebElement button=browserObject.findElement(By.className("button-1"));
 	// 11. Submit the button.
         button.submit();
 	// 12. Close the web browser.
